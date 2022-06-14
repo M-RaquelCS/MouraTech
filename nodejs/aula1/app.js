80e27dd7b8d7193881796b74b48ea9625bccfc61
raquel <m-raquel@outlook.com.br>
2022-06-14 20:07:22 -0300

    envio de email com Nodemailer e Mailtrap

---
+++ nodejs/aula1/app.js
@@ -0,0 +1,118 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+var app = express();             
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+ 
+// NODEMAILER
+
+var transporter = nodemailer.createTransport({
+  host: process.env.NODEMAILER_HOST,
+  port: 2525,
+  auth: {
+    user: process.env.AUTH_USER,
+    pass: process.env.AUTH_PASSWORD
+  }  
+});
+ 
+app.use('/contact/send', async function(req, res) {
+  
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Esqueceu ✔", // Subject line
+    text: "Esqueceu senha?", // plain text body
+    html: "<b>Esqueceu senha?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+} )
+
+app.use('/login', function(req, res) {
+  
+  // send mail with defined transport object
+  let info = transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Login ✔", // Subject line
+    text: "Login", // plain text body
+    html: "<b>Login</b>", // html body
+  });   
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+} )
+
+app.use('/register', function(req, res) {
+  
+  // send mail with defined transport object
+  let info = transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Register ✔", // Subject line
+    text: "Register", // plain text body
+    html: "<b>Register</b>", // html body
+  });   
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+} )
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+app.listen(3000, function(){
+  console.log('listening on port 3000');
+})
+module.exports = app;
